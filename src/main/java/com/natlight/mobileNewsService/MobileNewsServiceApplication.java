dfb5f640407f0b70f86ee1eb1e32214a1bbc1caa
Nataliia <novykovanatalie@gmail.com>
2020-02-12 20:37:46 +0100

    Add empty Springboot application

---
+++ src/main/java/com/natlight/mobileNewsService/MobileNewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.natlight.mobileNewsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileNewsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileNewsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natlight/mobileNewsService/MobileNewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natlight.mobileNewsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileNewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
